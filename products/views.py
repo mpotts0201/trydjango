82a98cdcafb619c3d0f340b729f43920707e5a83
mpotts0201 <mpotts0201@gmail.com>
2019-05-06 13:15:05 -0400

    Serving data from db in views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def product_detail_view(request, *args, **kwargs):
+    obj = Product.objects.get(id=3)
+
+    print("REQUEST: ", request)
+
+    # context = {
+    #     'title': obj.title,
+    #     'description': obj.description
+    # }
+
+    context = { 
+        'object': obj
+    }
+    return render(request, "product/detail.html", context)
